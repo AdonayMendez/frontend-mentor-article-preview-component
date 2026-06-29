bd6f37de0a53d03e3b6015f9b3e3d8ff4fe3e642
Adonay Mendez <adonay.mendez255@gmail.com>
2026-06-29 05:49:01 -0400

    Update share popup layout and styling

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let shareBtn = document.getElementById("shareBtn"); 
 let sharePopUp = document.querySelector(".share-popup"); 
+let shareContainer = document.querySelector(".share-container"); //
 
 
 shareBtn.addEventListener('click', () =>{
   sharePopUp.classList.toggle("show");
+  shareContainer.classList.toggle("active");
 });
